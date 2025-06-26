9c8b54a0d0c1a515b97af6223c7a67bd3a394f08
Roberto Barrera <132605956+RobertoBarre25@users.noreply.github.com>
2025-06-26 13:11:17 -0600

    Update vite.config.js

--- front_ApiAutor/vite.config.js
+++ front_ApiAutor/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' 
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: true,
+    port: process.env.PORT,
+    allowedHosts: ['front-apiautor.onrender.com']
+  }
 })

e9e78eb701c5f6b0c783eb6774a8d1e21701c79e
Roberto Barrera <robegamer25@gmail.com>
2025-07-08 23:06:34 -0600

    Create Input.jsx

---
+++ front_ApiAutor/src/components/Input.jsx
@@ -0,0 +1,11 @@
+import './Input.css';
+
+const Input = ({ label, ...props }) => (
+  <div className="input-group">
+    <label className="input-label">{label}</label>
+    <input className="input-field" {...props} />
+  </div>
+);
+
+export default Input;
+

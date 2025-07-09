9f36baa6a5af0e35d6128174119e2fb5e554d7a9
Roberto Barrera <robegamer25@gmail.com>
2025-07-08 23:02:59 -0600

    Update App.jsx

--- front_ApiAutor/src/App.jsx
+++ front_ApiAutor/src/App.jsx
@@ -1,8 +1,22 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/registro" element={<Register />} />
+        <Route path="/recuperar" element={<ForgotPassword />} />
+        <Route path="/cambiar" element={<ResetPassword />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
